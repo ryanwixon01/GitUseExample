0b28539bfbab3d90352defa2087c6450e35a410b
Ryan Wixon <ryanwixon02@gmail.com>
2020-09-30 11:56:19 -0500

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Wixon, Ryan P on 9/30/20.
+//  Copyright © 2020 Wixon, Ryan P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
